ed1de05f9abbbd62a6a31eec75df6116ba18a36e
Sakshath Rai K <90779367+sakshathrai@users.noreply.github.com>
2023-03-20 01:36:11 +0530

    Add files via upload

---
+++ Box.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Box{
+double width;
+double height;
+double depth;
+
+Box(double w, double h,double d){
+width=w;
+height=h;
+depth=d;
+}
+
+Box(){
+width=-1;
+height=-1;
+depth=-1;
+}
+
+Box(double len){
+width=height=depth=len;
+}
+
+double volume(){
+return width*height*depth;
+}
+
+public static void main(String args[]){
+Box mybox1=new Box(10,20,15);
+Box mybox2=new Box();
+Box mycube=new Box(7);
+double vol;
+vol=mybox1.volume();
+System.out.println("Volume of Box1 is "+vol);
+vol=mybox2.volume();
+System.out.println("Volume of unknown dimension is "+vol);
+vol=mycube.volume();
+System.out.println("Volume of Cube is "+vol);
+}
+}
